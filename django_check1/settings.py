1a9219e914258bda58a08c01406c6bb695b57cdc
Monika Patel <moni@Monikas-MacBook-Air.local>
2023-06-13 12:13:10 -0400

    second commit

--- django_check1/settings.py
+++ django_check1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--ksu6llk%)+uz%n00dtsl+f(jbox&_sd0^^l!c8$iungd*-)qi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
